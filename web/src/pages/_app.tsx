7a9dfa06cc34034bc3a4e8ab99806221964589ee
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-02-22 20:26:27 -0300

    config: react toastify

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,11 +1,16 @@
-import AppProvider from "@/contexts";
+import { ToastContainer } from "react-toastify";
+
 import GlobalStyle from "@styles/globals";
 import type { AppProps } from "next/app";
+import "react-toastify/dist/ReactToastify.css";
+
+import AppProvider from "@/contexts";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppProvider>
       <GlobalStyle />
+      <ToastContainer autoClose={3000} />
       <Component {...pageProps} />
     </AppProvider>
   );
