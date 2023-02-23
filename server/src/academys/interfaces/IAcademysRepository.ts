5fb5c848218f77914645c065d77d2915b58f1378
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-02-23 17:17:52 -0300

    feat: adds abstract method create to IAcademysRepository

--- server/src/academys/interfaces/IAcademysRepository.ts
+++ server/src/academys/interfaces/IAcademysRepository.ts
@@ -1,5 +1,8 @@
+import { CreateAcademyDto } from '@academys/dto/create-academy.dto';
 import { Academy } from '@academys/infra/typeorm/entities/academy.entity';
 
 export abstract class IAcademysRepository {
+  abstract create(data: CreateAcademyDto): Promise<Academy>;
+
   abstract findById(id: string): Promise<Academy>;
 }
