50682291ac0dc1cae6b2a0aa5bd2d55ff85ddc5c
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-02-23 17:05:38 -0300

    feat: creates academys controller with findById route

---
+++ server/src/shared/infra/http/controllers/academys.controllers.ts
@@ -0,0 +1,12 @@
+import { Academy } from '@academys/infra/typeorm/entities/academy.entity';
+import { Controller, Get, Param } from '@nestjs/common';
+import { FindByIdUseCase } from './../../../../academys/use-cases/findById/FindByIdUseCase';
+@Controller('academys')
+export class AcademysControllers {
+  constructor(private findByIdUseCase: FindByIdUseCase) {}
+
+  @Get('academys/:id')
+  findById(@Param('id') id: string): Promise<Academy> {
+    return this.findByIdUseCase.execute(id);
+  }
+}
