140788840801f68088fe8a362d9686fa96f458a5
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-02-23 16:53:09 -0300

    feat: adds create method to inMemoryRepository

--- server/src/academys/test/in-memory/InMemoryAcademysRepository.ts
+++ server/src/academys/test/in-memory/InMemoryAcademysRepository.ts
@@ -1,7 +1,19 @@
-import { Academy } from 'src/academys/infra/typeorm/entities/academy.entity';
-import { IAcademysRepository } from 'src/academys/interfaces';
+import { CreateAcademyDto } from '@academys/dto/create-academy.dto';
+import { Academy } from '@academys/infra/typeorm/entities/academy.entity';
+import { IAcademysRepository } from '@academys/interfaces';
+
 export class InMemoryAcademysRepository implements IAcademysRepository {
   academys: Academy[] = [];
+
+  async create(data: CreateAcademyDto): Promise<Academy> {
+    const academy = new Academy();
+
+    Object.assign(academy, data);
+
+    this.academys.push(academy);
+
+    return academy;
+  }
 
   async findById(id: string): Promise<Academy> {
     return this.academys.find((academy) => academy.id === id);
