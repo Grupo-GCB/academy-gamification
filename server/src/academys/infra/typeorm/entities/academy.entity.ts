debcab8f1e4f627a4373b1d2379e3d10b3a5c1bc
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-02-23 14:21:42 -0300

    feat: adds properties and orm decoratos to entity

--- server/src/academys/infra/typeorm/entities/academy.entity.ts
+++ server/src/academys/infra/typeorm/entities/academy.entity.ts
@@ -1,1 +1,13 @@
-export class Academy {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('academys')
+export class Academy {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+}
