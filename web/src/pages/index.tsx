fa436ce8d1936f828807f7774b208ee2fcc5ed0c
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-02-23 17:22:25 -0300

    feat: create list with anchor link to pages

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,10 +1,18 @@
-import { Button } from "@/components";
-
 export default function Home() {
   return (
     <div>
-      <h1>Oi</h1>
-      <Button>Botão</Button>
+      <h1>Home - Gamification</h1>
+      <ul>
+        <li>
+          <a href="/academy">Academy</a>
+        </li>
+        <li>
+          <a href="/collaborator">Collaborator</a>
+        </li>
+        <li>
+          <a href="/admin">Admin</a>
+        </li>
+      </ul>
     </div>
   );
 }
