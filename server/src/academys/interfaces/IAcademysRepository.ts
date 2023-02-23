e477f087a0604f43ffee033f828847c68b0d47ab
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-02-23 14:31:52 -0300

    feat: adds abstract method findById to IAcademysRepository

--- server/src/academys/interfaces/IAcademysRepository.ts
+++ server/src/academys/interfaces/IAcademysRepository.ts
@@ -1,1 +1,4 @@
-export abstract class IAcademysRepository {}
+import { Academy } from './../infra/typeorm/entities/academy.entity';
+export abstract class IAcademysRepository {
+  abstract findById(id: string): Promise<Academy>;
+}
