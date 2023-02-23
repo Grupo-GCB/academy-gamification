185e9f9adf03fe141c1740b24ec6bee65284c800
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-02-23 14:36:04 -0300

    feat: implements method to find a academy by his Id

--- server/src/academys/use-cases/findById/FindByIdUseCase.ts
+++ server/src/academys/use-cases/findById/FindByIdUseCase.ts
@@ -1,1 +1,17 @@
-export class FindByIdUseCase {}
+import { Academy } from './../../infra/typeorm/entities/academy.entity';
+import { IAcademysRepository } from 'src/academys/interfaces/IAcademysRepository';
+import { NotFoundException } from '@nestjs/common';
+
+export class FindByIdUseCase {
+  constructor(private academysRepository: IAcademysRepository) {}
+
+  async execute(id: string): Promise<Academy> {
+    const academy = this.academysRepository.findById(id);
+
+    if (!academy) {
+      throw new NotFoundException('Academy does not exist');
+    }
+
+    return academy;
+  }
+}
