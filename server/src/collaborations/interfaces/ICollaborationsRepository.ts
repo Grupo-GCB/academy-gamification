46f7024473c66ff7dad15f2f16bb0f55e83efaae
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-02-24 15:39:09 -0300

    chore: changes method parameters

--- server/src/collaborations/interfaces/ICollaborationsRepository.ts
+++ server/src/collaborations/interfaces/ICollaborationsRepository.ts
@@ -1,12 +1,12 @@
-import { CollaborationsStatus } from './../infra/typeorm/entities/collaboration.entity';
 import { Collaboration } from '@collaborations/infra/typeorm/entities/collaboration.entity';
 import { CreateCollaborationDto } from '@collaborations/dto/create-collaboration.dto';
+import { FilterCollaborationByStatusDto } from '@collaborations/dto/filter-collaboration-by-status.dto';
 
 export abstract class ICollaborationsRepository {
   abstract create(data: CreateCollaborationDto): Promise<Collaboration>;
 
-  abstract filterCollaborationsByStatus(
-    status: CollaborationsStatus,
-    id: string,
-  ): Promise<Collaboration[]>;
+  abstract filterCollaborationsByStatus({
+    status,
+    id,
+  }: FilterCollaborationByStatusDto): Promise<Collaboration[]>;
 }
