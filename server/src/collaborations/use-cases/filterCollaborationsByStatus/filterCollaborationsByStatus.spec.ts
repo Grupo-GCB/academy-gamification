6074b93c84703ebc9cb5165372cb8dc6f386cbcc
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-02-25 17:56:12 -0300

    chore: inserts better ids to tests

--- server/src/collaborations/use-cases/filterCollaborationsByStatus/filterCollaborationsByStatus.spec.ts
+++ server/src/collaborations/use-cases/filterCollaborationsByStatus/filterCollaborationsByStatus.spec.ts
@@ -1,7 +1,7 @@
 import { FilterCollaborationsByStatus } from './filter-collaborations-by-status';
 import { InMemoryCollaborationsRepository } from '@collaborations/test/in-memory/inMemoryCollaborationsRepository';
 import { NotFoundException } from '@nestjs/common';
-import { CollaborationsStatus } from 'shared/constants';
+import { CollaborationsStatus } from '@shared/constants';
 
 describe('Find Collaboration by Academy id', () => {
   let inMemoryCollaborationRepository: InMemoryCollaborationsRepository;
@@ -16,22 +16,22 @@ describe('Find Collaboration by Academy id', () => {
 
   it('should find academy with passed collaboration status passed', async () => {
     const collaboration1 = await inMemoryCollaborationRepository.create({
-      collaboration_type_id: '123456',
-      collaborator_id: '123456',
-      academy_id: 'academy123456',
+      collaboration_type_id: '0a1b5baa-a8dd-4aff-90cd-5accc7a266f0',
+      collaborator_id: 'f8007e10-b750-4e24-9342-21c1f51e1f99',
+      academy_id: 'bb324053-cfe0-49ad-9c24-1d6bd368555e',
       status: CollaborationsStatus.pending,
     });
 
     const collaboration2 = await inMemoryCollaborationRepository.create({
-      collaboration_type_id: '654321',
-      collaborator_id: '123456',
-      academy_id: 'academy123456',
+      collaboration_type_id: 'e802eade-11c6-4c94-9e0b-37dfdfd67c87',
+      collaborator_id: '3ed27bd9-9a87-4f75-9a84-17e1949311d3',
+      academy_id: 'bb324053-cfe0-49ad-9c24-1d6bd368555e',
       status: CollaborationsStatus.pending,
     });
 
     const pendingCollaborations = await filterCollaborationsByStatus.execute({
       status: CollaborationsStatus.pending,
-      academy_id: 'academy123456',
+      academy_id: 'bb324053-cfe0-49ad-9c24-1d6bd368555e',
     });
 
     expect(pendingCollaborations).toEqual([collaboration1, collaboration2]);
@@ -39,9 +39,9 @@ describe('Find Collaboration by Academy id', () => {
 
   it('should throw an error if academy does not have collaborations with passed status', async () => {
     const collaboration = await inMemoryCollaborationRepository.create({
-      collaboration_type_id: '654321',
-      collaborator_id: '123456',
-      academy_id: 'academy123456',
+      collaboration_type_id: '3b581175-13f2-471a-bda8-f08987ca9506',
+      collaborator_id: '012f000f-bcdb-4c5f-b1bf-d917dac65426',
+      academy_id: '94a3635a-6bcd-4894-a3e1-98d76150e3ca',
       status: CollaborationsStatus.pending,
     });
 
