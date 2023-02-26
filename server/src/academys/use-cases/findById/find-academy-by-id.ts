c10c1dc2cb519deec063c84d11dd5366d1b6cf14
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-02-26 18:58:18 -0300

    feat: adds validation if id is passed

--- server/src/academys/use-cases/findById/find-academy-by-id.ts
+++ server/src/academys/use-cases/findById/find-academy-by-id.ts
@@ -1,4 +1,8 @@
-import { NotFoundException, Injectable } from '@nestjs/common';
+import {
+  NotFoundException,
+  Injectable,
+  BadRequestException,
+} from '@nestjs/common';
 
 import { Academy } from '@academys/infra/typeorm/entities/academy.entity';
 import { IAcademysRepository } from '@academys/interfaces';
@@ -8,6 +12,8 @@ export class FindByIdUseCase {
   constructor(private academysRepository: IAcademysRepository) {}
 
   async execute(id: string): Promise<Academy> {
+    if (!id) throw new BadRequestException('Id is required');
+
     const academy = await this.academysRepository.findById(id);
 
     if (!academy) throw new NotFoundException('Academy does not exist');
