37576647e1798834bedbe980f8e2cb4d811b2fae
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-02-27 14:46:17 -0300

    feat: adds validation id academy exists

--- server/src/collaborations/use-cases/filterByAcademyAndStatus/filter-by-academy-and-status.ts
+++ server/src/collaborations/use-cases/filterByAcademyAndStatus/filter-by-academy-and-status.ts
@@ -2,17 +2,26 @@ import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 import { Collaboration } from '@collaborations/infra/typeorm/entities/collaboration.entity';
 import { ICollaborationsRepository } from '@collaborations/interfaces';
-import { FilterByAcademyAndStatusDTO } from '@collaborations/dto/filter-by-academy-and-status';
+import { FilterByAcademyAndStatusDTO } from '@collaborations/dto';
+import { IAcademysRepository } from '@academys/interfaces';
 
 export class FilterByAcademyAndStatus {
-  constructor(private collaborationsRepository: ICollaborationsRepository) {}
+  constructor(
+    private collaborationsRepository: ICollaborationsRepository,
+    private academysRepository: IAcademysRepository,
+  ) {}
 
   async execute({
     status,
     academy_id,
   }: FilterByAcademyAndStatusDTO): Promise<Collaboration[]> {
+    const academy = await this.academysRepository.findById(academy_id);
+
+    if (!academy) throw new NotFoundException('Academy not found');
+
     if (!status || !academy_id)
       throw new BadRequestException('Id and Status are required!');
+
     const pendingCollaborations: Collaboration[] =
       await this.collaborationsRepository.filterByAcademyAndStatus({
         status,
