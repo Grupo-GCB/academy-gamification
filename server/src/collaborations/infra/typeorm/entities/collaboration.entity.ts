7c19a4513c4c846ba613e3110101e1a281f836d1
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-02-28 16:22:11 -0300

    feat: creates Collaboration entity

--- server/src/collaborations/infra/typeorm/entities/collaboration.entity.ts
+++ server/src/collaborations/infra/typeorm/entities/collaboration.entity.ts
@@ -1,1 +1,41 @@
-export class Collaboration {}
+import { randomUUID } from 'node:crypto';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { CollaborationsStatus } from '@shared/constants';
+
+@Entity('collaboration')
+export class Collaboration {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  type: string;
+
+  @Column()
+  url: string;
+
+  @Column()
+  collaborator_id: string;
+
+  @Column()
+  status: CollaborationsStatus;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  approved_at?: Date = null;
+
+  @UpdateDateColumn()
+  rejected_at?: Date = null;
+
+  constructor() {
+    if (!this.id) this.id = randomUUID();
+  }
+}
