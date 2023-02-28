094088065d24bf0c3129a9d3126c243a4053e050
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-02-28 17:06:54 -0300

    feat: implements method method to find all collaborations that math the status passed

--- server/src/collaborations/use-cases/findCollaborationsByStatus/find-collaborations-by-status.ts
+++ server/src/collaborations/use-cases/findCollaborationsByStatus/find-collaborations-by-status.ts
@@ -1,8 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
 
 import { ICollaborationsRepository } from '@collaborations/interfaces';
+import { Collaboration } from '@collaborations/infra/typeorm/entities/collaboration.entity';
+import { CollaborationsStatus } from '@shared/constants';
 
 @Injectable()
 export class FindByStatus {
   constructor(private collaborationsRepository: ICollaborationsRepository) {}
+
+  async execute(status: CollaborationsStatus): Promise<Collaboration[]> {
+    if (!status) throw new BadRequestException('Status is required');
+
+    const collaborations: Collaboration[] =
+      await this.collaborationsRepository.findByStatus(
+        CollaborationsStatus.PENDING,
+      );
+
+    if (!collaborations) throw new NotFoundException('No collaborations found');
+
+    return collaborations;
+  }
 }
