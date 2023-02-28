1cef8e4f17c5a802df72fa4b7dd55c46fc79bf02
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-02-28 16:42:19 -0300

    feat: creates findByStatus on ICollaborationsRepository

--- server/src/collaborations/interfaces/ICollaborationsRepository.ts
+++ server/src/collaborations/interfaces/ICollaborationsRepository.ts
@@ -1,6 +1,9 @@
+import { CollaborationsStatus } from '@shared/constants';
 import { RegisterCollaborationDTO } from '@collaborations/dto/register-collaboration.dto';
 import { Collaboration } from '@collaborations/infra/typeorm/entities/collaboration.entity';
 
 export abstract class ICollaborationsRepository {
   abstract register(data: RegisterCollaborationDTO): Promise<Collaboration>;
+
+  abstract findByStatus(status: CollaborationsStatus): Promise<Collaboration[]>;
 }
