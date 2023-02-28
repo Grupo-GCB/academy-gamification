31ef7ed3a40acd8cbb87392c0ff60a2cdec4a535
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-02-28 16:50:58 -0300

    feat: implements register and findByStatus at InMemoryRepository

---
+++ server/src/collaborations/test/in-memory/InMemoryCollaborationsRepository.ts
@@ -0,0 +1,26 @@
+import { RegisterCollaborationDTO } from '@collaborations/dto/register-collaboration.dto';
+import { Collaboration } from '@collaborations/infra/typeorm/entities/collaboration.entity';
+import { ICollaborationsRepository } from '@collaborations/interfaces';
+import { CollaborationsStatus } from '@shared/constants';
+
+export class InMemoryCollaborationsRepository
+  implements ICollaborationsRepository
+{
+  collaborations: Collaboration[] = [];
+
+  async register(data: RegisterCollaborationDTO): Promise<Collaboration> {
+    const collaboration = Object.assign(new Collaboration(), data);
+
+    this.collaborations.push(collaboration);
+
+    return collaboration;
+  }
+
+  async findByStatus(status: CollaborationsStatus): Promise<Collaboration[]> {
+    const collaborations = this.collaborations.filter(
+      (collaboration) => collaboration.status === status,
+    );
+
+    return collaborations;
+  }
+}
