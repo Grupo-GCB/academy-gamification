0a8f7f23d601b90249e9eca4984b11e4c4916913
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-02-28 18:33:05 -0300

    test: tests if return collaborations with status passed

--- server/src/collaborations/use-cases/findCollaborationsByStatus/find-collaborations-by-status.spec.ts
+++ server/src/collaborations/use-cases/findCollaborationsByStatus/find-collaborations-by-status.spec.ts
@@ -39,4 +39,24 @@ describe('Find collaborations by status', () => {
       findByStatus.execute(CollaborationsStatus.APPROVED),
     ).rejects.toEqual(new NotFoundException('No collaborations found'));
   });
+
+  it('should return all collaborations that matches status passed', async () => {
+    const collaboration1 = await inMemoryCollaborationsRepository.register({
+      type: 'Logic Exercise',
+      url: 'www.notion.so/logicexercise',
+      collaborator_id: 'f8007e10-b750-4e24-9342-21c1f51e1f99',
+      status: CollaborationsStatus.PENDING,
+    });
+
+    const collaboration2 = await inMemoryCollaborationsRepository.register({
+      type: 'Code Review',
+      url: 'github.com/repoName',
+      collaborator_id: '69de5f11-6b66-45df-bf92-a633dc3382c7',
+      status: CollaborationsStatus.PENDING,
+    });
+
+    const sut = await findByStatus.execute(CollaborationsStatus.PENDING);
+
+    await expect(sut).toEqual([collaboration1, collaboration2]);
+  });
 });
