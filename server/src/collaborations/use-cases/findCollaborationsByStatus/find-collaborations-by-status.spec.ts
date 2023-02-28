0f05ec7a45fbf0eaa56ebc430f43a4513e1e7372
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-02-28 18:32:21 -0300

    test: tests if throw error if no collaboration status passed is found

--- server/src/collaborations/use-cases/findCollaborationsByStatus/find-collaborations-by-status.spec.ts
+++ server/src/collaborations/use-cases/findCollaborationsByStatus/find-collaborations-by-status.spec.ts
@@ -1,7 +1,8 @@
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 import { FindByStatus } from '@collaborations/use-cases';
 import { InMemoryCollaborationsRepository } from '@collaborations/test/in-memory/InMemoryCollaborationsRepository';
+import { CollaborationsStatus } from '@shared/constants';
 
 describe('Find collaborations by status', () => {
   let inMemoryCollaborationsRepository: InMemoryCollaborationsRepository;
@@ -13,8 +14,29 @@ describe('Find collaborations by status', () => {
   });
 
   it('should throw an error if status is not passed', async () => {
-    await expect(findByStatus.execute(null)).rejects.toEqual(
+    const sut = findByStatus.execute(null);
+    await expect(sut).rejects.toEqual(
       new BadRequestException('Status is required'),
     );
+  });
+
+  it('should throw an error if no collaboration with status passed is found', async () => {
+    const collaboration1 = await inMemoryCollaborationsRepository.register({
+      type: 'Logic Exercise',
+      url: 'www.notion.so/logicexercise',
+      collaborator_id: 'f8007e10-b750-4e24-9342-21c1f51e1f99',
+      status: CollaborationsStatus.PENDING,
+    });
+
+    const collaboration2 = await inMemoryCollaborationsRepository.register({
+      type: 'Code Review',
+      url: 'github.com/repoName',
+      collaborator_id: '69de5f11-6b66-45df-bf92-a633dc3382c7',
+      status: CollaborationsStatus.PENDING,
+    });
+
+    await expect(
+      findByStatus.execute(CollaborationsStatus.APPROVED),
+    ).rejects.toEqual(new NotFoundException('No collaborations found'));
   });
 });
