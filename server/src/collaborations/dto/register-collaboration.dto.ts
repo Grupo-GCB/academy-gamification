740b4b854201f8ee413d8897217cbd38483cc228
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-02-28 16:28:59 -0300

    feat: implements register collaboration dto

--- server/src/collaborations/dto/register-collaboration.dto.ts
+++ server/src/collaborations/dto/register-collaboration.dto.ts
@@ -1,1 +1,21 @@
-export class RegisterCollaborationDto {}
+import { IsEnum, IsNotEmpty, IsString, IsUrl } from 'class-validator';
+
+import { CollaborationsStatus } from '@shared/constants';
+
+export class RegisterCollaborationDTO {
+  @IsNotEmpty()
+  @IsString()
+  type: string;
+
+  @IsNotEmpty()
+  @IsUrl()
+  url: string;
+
+  @IsNotEmpty()
+  @IsString()
+  collaborator_id: string;
+
+  @IsNotEmpty()
+  @IsEnum(CollaborationsStatus)
+  status: CollaborationsStatus;
+}
