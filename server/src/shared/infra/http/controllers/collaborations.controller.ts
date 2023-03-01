055d0cb6c99e9af6792dc58516acbcbca3e37d30
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-01 16:28:36 -0300

    feat: implements filterByStatus on controller

--- server/src/shared/infra/http/controllers/collaborations.controller.ts
+++ server/src/shared/infra/http/controllers/collaborations.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Query } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+
 import { FindByStatus } from '@collaborations/use-cases';
 
 @Controller('collaborations')
 export class CollaborationsController {
-  constructor(private FindByStatus: FindByStatus) {}
+  constructor(private findByStatus: FindByStatus) {}
 
   @Get()
-  filterByStatus(@Query('status') status: CollaborationsStatus);
+  filterByStatus(@Query('status') status: string) {
+    return this.findByStatus.execute(status);
+  }
 }
