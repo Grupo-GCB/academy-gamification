f913eb00def4c94a6b5ab8a7c97d544292a17d5e
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-03 17:15:49 -0300

    feat: adds findOne and UpdateStatus to providers

--- server/src/collaborations/collaborations.module.ts
+++ server/src/collaborations/collaborations.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Collaboration } from '@collaborations/infra/typeorm/entities/collaboration.entity';
-import { FindByStatus } from '@collaborations/use-cases';
+import { FindByStatus, FindOne, UpdateStatus } from '@collaborations/use-cases';
 import { CollaborationsController } from '@shared/infra/http/controllers/collaborations.controller';
 import { ICollaborationsRepository } from '@collaborations/interfaces';
 import { CollaborationsRepository } from '@collaborations/infra/typeorm/repositories/collaborations.repository';
@@ -12,6 +12,8 @@ import { CollaborationsRepository } from '@collaborations/infra/typeorm/reposito
   controllers: [CollaborationsController],
   providers: [
     FindByStatus,
+    FindOne,
+    UpdateStatus,
     {
       provide: ICollaborationsRepository,
       useClass: CollaborationsRepository,
