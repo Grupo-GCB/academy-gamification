4fad4dd7643a23ae7c3c905854cd78cd0a7aea9a
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-03 13:37:42 -0300

    feat: implements use case to find one collaboration

--- server/src/collaborations/use-cases/findById/find-by-id.ts
+++ server/src/collaborations/use-cases/findById/find-by-id.ts
@@ -1,1 +1,19 @@
-export class FindById {}
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { Collaboration } from '@collaborations/infra/typeorm/entities/collaboration.entity';
+import { ICollaborationsRepository } from '@collaborations/interfaces';
+
+@Injectable()
+export class FindById {
+  constructor(private collaborationsRepository: ICollaborationsRepository) {}
+
+  async execute(collaboration_id: string): Promise<Collaboration> {
+    const collaboration: Collaboration =
+      await this.collaborationsRepository.findOne(collaboration_id);
+
+    if (!collaboration)
+      throw new NotFoundException('Collaborations does not exist');
+
+    return collaboration;
+  }
+}
