cf15968ddd019583177daf8d71488d7c825b050a
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-03 19:07:06 -0300

    chore: adds swagger documentations

--- server/src/collaborations/dto/update-status.dto.ts
+++ server/src/collaborations/dto/update-status.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 
 import { CollaborationsStatus } from '@shared/constants';
@@ -5,9 +6,21 @@ import { CollaborationsStatus } from '@shared/constants';
 export class UpdateStatusDTO {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    example: '0cb9f40a-a173-4a40-921f-c492c95cd57b',
+    description: 'Id da colaboração',
+    type: 'string',
+    required: true,
+  })
   collaboration_id: string;
 
   @IsNotEmpty()
   @IsEnum(CollaborationsStatus)
+  @ApiProperty({
+    example: 'APPROVED',
+    description: 'Status que vai ser atualizado na colaboração',
+    type: 'CollaborationStatus',
+    required: true,
+  })
   newStatus: CollaborationsStatus;
 }
