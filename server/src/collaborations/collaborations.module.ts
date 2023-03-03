e17c06311f87bc93e1306933ed5153935e61bed0
GuWuelta <rm93449@fiap.com.br>
2023-03-03 18:05:26 -0300

    feat: imports register collaboration

--- server/src/collaborations/collaborations.module.ts
+++ server/src/collaborations/collaborations.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Collaboration } from '@collaborations/infra/typeorm/entities/collaboration.entity';
-import { FindByStatus } from '@collaborations/use-cases';
+import { FindByStatus, RegisterCollaboration } from '@collaborations/use-cases';
 import { CollaborationsController } from '@shared/infra/http/controllers/collaborations.controller';
 import { ICollaborationsRepository } from '@collaborations/interfaces';
 import { CollaborationsRepository } from '@collaborations/infra/typeorm/repositories/collaborations.repository';
@@ -12,6 +12,7 @@ import { CollaborationsRepository } from '@collaborations/infra/typeorm/reposito
   controllers: [CollaborationsController],
   providers: [
     FindByStatus,
+    RegisterCollaboration,
     {
       provide: ICollaborationsRepository,
       useClass: CollaborationsRepository,
