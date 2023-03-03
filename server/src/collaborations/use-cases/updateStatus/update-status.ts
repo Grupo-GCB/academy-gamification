888eeb7dc498e644b9c7f949ae247ec7542da1a1
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-03 12:07:48 -0300

    refactor: refactors update status

--- server/src/collaborations/use-cases/updateStatus/update-status.ts
+++ server/src/collaborations/use-cases/updateStatus/update-status.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
-import { UpdateStatusDTO } from 'src/collaborations/dto/update-status.dto';
-import { Collaboration } from 'src/collaborations/infra/typeorm/entities/collaboration.entity';
+import { UpdateStatusDTO } from '@collaborations/dto';
+import { Collaboration } from '@collaborations/infra/typeorm/entities/collaboration.entity';
+import { ICollaborationsRepository } from '@collaborations/interfaces';
 
 @Injectable()
 export class UpdateStatus {
@@ -11,21 +12,12 @@ export class UpdateStatus {
     collaboration_id,
     newStatus,
   }: UpdateStatusDTO): Promise<Collaboration> {
-    const collaboration = await this.collaborationsRepository.findById(
-      collaboration_id,
-    );
+    const collaboration: Collaboration =
+      await this.collaborationsRepository.findOne(collaboration_id);
 
     if (!collaboration) {
       throw new Error('Collaboration not found!');
     }
-
-    const isValidStatus = newStatus.includes('enum');
-
-    if (!isValidStatus) {
-      throw new Error('Invalid status!');
-    }
-
-    collaboration.status = newStatus;
 
     return this.collaborationsRepository.updateStatus({
       collaboration_id,
