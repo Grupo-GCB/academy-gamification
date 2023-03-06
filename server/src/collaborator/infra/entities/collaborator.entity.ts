a83bd56086077a46f620d885f577c96bdc6dd633
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-06 18:03:40 -0300

    feat: creates collaboration entity

--- server/src/collaborator/infra/entities/collaborator.entity.ts
+++ server/src/collaborator/infra/entities/collaborator.entity.ts
@@ -1,1 +1,18 @@
-export class Collaborator {}
+import { randomUUID } from 'node:crypto';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('collaborator')
+export class Collaborator {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  constructor() {
+    if (!this.id) this.id = randomUUID();
+  }
+}
