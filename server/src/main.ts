f53263bbc999d94ec6ae6e2fc544e0fa44c08d49
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-06 17:40:57 -0300

    chore: adds globalPipes

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
@@ -5,6 +6,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Academy-Gamification')
