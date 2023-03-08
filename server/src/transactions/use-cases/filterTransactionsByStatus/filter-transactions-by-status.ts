a4477302a5bdf48226e501a68e8257a8c18d17ca
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-08 09:41:54 -0300

    feat: creates filter-transactions-by-status use case

---
+++ server/src/transactions/use-cases/filterTransactionsByStatus/filter-transactions-by-status.ts
@@ -0,0 +1,21 @@
+import { Injectable, BadRequestException } from '@nestjs/common';
+
+import { ITransactionsRepository } from '@transactions/interfaces';
+import { Transaction } from '@transactions/infra/typeorm/entities/transaction.entity';
+import { FilterTransactionsByStatusDTO } from '@transactions/dto';
+
+@Injectable()
+export class FilterTransacitonsByStatus {
+  constructor(private transactionsRepository: ITransactionsRepository) {}
+
+  async execute({
+    status,
+  }: FilterTransactionsByStatusDTO): Promise<Transaction[]> {
+    if (!status) throw new BadRequestException('Status is required');
+
+    const transactions: Transaction[] =
+      await this.transactionsRepository.filterByStatus({ status });
+
+    return transactions;
+  }
+}
