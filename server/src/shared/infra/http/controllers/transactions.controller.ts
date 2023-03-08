c3e07622d65f31786d0a61a3d29ecd98839af9ac
GuWuelta <rm93449@fiap.com.br>
2023-03-08 16:44:16 -0300

    feat: adds update and find by id routes

--- server/src/shared/infra/http/controllers/transactions.controller.ts
+++ server/src/shared/infra/http/controllers/transactions.controller.ts
@@ -1,13 +1,34 @@
-import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
-import { ApiBadRequestResponse, ApiCreatedResponse } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+} from '@nestjs/swagger';
 
+import { RegisterTransactionDTO, UpdateStatusDTO } from '@transactions/dto';
 import { Transaction } from '@transactions/infra/typeorm/entities/transaction.entity';
-import { RegisterTransactionDTO } from '@transactions/dto';
-import { RegisterTransaction } from '@transactions/use-cases';
+import {
+  FindById,
+  RegisterTransaction,
+  UpdateStatus,
+} from '@transactions/use-cases';
 
 @Controller('transactions')
 export class TransactionsController {
-  constructor(private registerTransaction: RegisterTransaction) {}
+  constructor(
+    private registerTransaction: RegisterTransaction,
+    private findById: FindById,
+    private updateTransactionStatus: UpdateStatus,
+  ) {}
 
   @Post('/register')
   @ApiCreatedResponse({
@@ -23,5 +44,35 @@ export class TransactionsController {
     data: RegisterTransactionDTO,
   ): Promise<Transaction> {
     return this.registerTransaction.execute(data);
+  }
+
+  @Get(':id')
+  @ApiOkResponse({
+    status: HttpStatus.OK,
+  })
+  @ApiNotFoundResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Não foi possível encontrar a transação',
+  })
+  findOne(@Param('id') id: string): Promise<Transaction> {
+    return this.findById.execute(id);
+  }
+
+  @Put()
+  @ApiOkResponse({
+    status: HttpStatus.OK,
+    description: 'Status da transação alterado com sucesso',
+  })
+  @ApiBadRequestResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Não foi possível alterar o status da transação',
+  })
+  updateStatus(
+    @Body() { transaction_id, newStatus }: UpdateStatusDTO,
+  ): Promise<Transaction> {
+    return this.updateTransactionStatus.execute({
+      transaction_id,
+      newStatus,
+    });
   }
 }
