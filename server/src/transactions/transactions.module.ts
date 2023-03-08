26db9e9b3214d577e89c9818c646322d46cab35f
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-08 14:11:49 -0300

    chore: adds filterByStatus provider

--- server/src/transactions/transactions.module.ts
+++ server/src/transactions/transactions.module.ts
@@ -1,17 +1,21 @@
-import { Transaction } from '@transactions/infra/typeorm/entities/transaction.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { TransactionsController } from '@shared/infra/http/controllers/transactions.controller';
-import { RegisterTransaction } from '@transactions/use-cases';
+import {
+  FilterTransactionsByStatus,
+  RegisterTransaction,
+} from '@transactions/use-cases';
 import { ITransactionsRepository } from '@transactions/interfaces';
 import { TransactionsRepository } from '@transactions/infra/typeorm/repositories/transactions.repository';
+import { Transaction } from '@transactions/infra/typeorm/entities/transaction.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Transaction])],
   controllers: [TransactionsController],
   providers: [
     RegisterTransaction,
+    FilterTransactionsByStatus,
     {
       provide: ITransactionsRepository,
       useClass: TransactionsRepository,
