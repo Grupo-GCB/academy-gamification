2723565064eb7d1a01fa6db2c6718e801ff0557c
GuWuelta <rm93449@fiap.com.br>
2023-03-08 16:45:12 -0300

    feat: adds update and find by id providers

--- server/src/transactions/transactions.module.ts
+++ server/src/transactions/transactions.module.ts
@@ -1,17 +1,23 @@
-import { Transaction } from '@transactions/infra/typeorm/entities/transaction.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Transaction } from '@transactions/infra/typeorm/entities/transaction.entity';
 
 import { TransactionsController } from '@shared/infra/http/controllers/transactions.controller';
-import { RegisterTransaction } from '@transactions/use-cases';
-import { ITransactionsRepository } from '@transactions/interfaces';
 import { TransactionsRepository } from '@transactions/infra/typeorm/repositories/transactions.repository';
+import { ITransactionsRepository } from '@transactions/interfaces';
+import {
+  FindById,
+  RegisterTransaction,
+  UpdateStatus,
+} from '@transactions/use-cases';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Transaction])],
   controllers: [TransactionsController],
   providers: [
     RegisterTransaction,
+    UpdateStatus,
+    FindById,
     {
       provide: ITransactionsRepository,
       useClass: TransactionsRepository,
