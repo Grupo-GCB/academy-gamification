fc684774d5d4295530f9b7c0328d83a48f214d7d
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-09 15:42:49 -0300

    feat: creates wallets entity

---
+++ server/src/wallet/infra/typeorm/entities/wallet.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { Collaborator } from '@collaborator/infra/entities/collaborator.entity';
+
+@Entity('wallets')
+export class Wallet {
+  @PrimaryColumn()
+  id: string;
+
+  @OneToOne(() => Collaborator, (collaborator) => collaborator.id)
+  @JoinColumn({ name: 'collaborator_id' })
+  @Column()
+  collaborator_id: string;
+
+  @Column()
+  gcbits: string;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
