3d20b693a20aa94ec06dd965cd7e141da89fe31e
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-09 16:08:54 -0300

    feat: adds method findOne to IWalletsRepository

--- server/src/wallet/interfaces/IWalletsRepository.ts
+++ server/src/wallet/interfaces/IWalletsRepository.ts
@@ -2,5 +2,6 @@ import { UpdateGcbitsDTO } from '@wallet/dto';
 import { Wallet } from '@wallet/infra/typeorm/entities/wallet.entity';
 
 export abstract class IWalletsRepository {
+  abstract findOne(id: string): Promise<Wallet>;
   abstract updateGcbits({ id, gcbits }: UpdateGcbitsDTO): Promise<Wallet>;
 }
