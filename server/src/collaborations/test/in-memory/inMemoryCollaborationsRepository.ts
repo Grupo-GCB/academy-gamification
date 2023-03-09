ab7453dfaf22c2f6b51818e65d6fb6ee1efc799e
GuWuelta <rm93449@fiap.com.br>
2023-03-09 14:10:56 -0300

    feat: implements methods

--- server/src/collaborations/test/in-memory/inMemoryCollaborationsRepository.ts
+++ server/src/collaborations/test/in-memory/inMemoryCollaborationsRepository.ts
@@ -1,14 +1,35 @@
-import { CreateCollaborationDto } from './../../dto/create-collaboration.dto';
+import {
+  FilterCollaborationsByStatusDTO,
+  RegisterCollaborationDTO,
+  UpdateStatusDTO,
+} from '@collaborations/dto';
+import { FilterCollaborationByStatusDto } from '@collaborations/dto/filter-collaboration-by-status.dto';
 import { Collaboration } from '@collaborations/infra/typeorm/entities/collaboration.entity';
 import { ICollaborationsRepository } from '@collaborations/interfaces';
-import { FilterCollaborationByStatusDto } from '@collaborations/dto/filter-collaboration-by-status.dto';
 
 export class InMemoryCollaborationsRepository
   implements ICollaborationsRepository
 {
+  register(data: RegisterCollaborationDTO): Promise<Collaboration> {
+    throw new Error('Method not implemented.');
+  }
+  filterByStatus({
+    status,
+  }: FilterCollaborationsByStatusDTO): Promise<Collaboration[]> {
+    throw new Error('Method not implemented.');
+  }
+  findOne(collaborations_id: string): Promise<Collaboration> {
+    throw new Error('Method not implemented.');
+  }
+  updateStatus({
+    collaboration_id,
+    newStatus,
+  }: UpdateStatusDTO): Promise<Collaboration> {
+    throw new Error('Method not implemented.');
+  }
   collaborations: Collaboration[] = [];
 
-  async create(data: CreateCollaborationDto): Promise<Collaboration> {
+  async create(data: RegisterCollaborationDTO): Promise<Collaboration> {
     const collaboration = new Collaboration();
 
     Object.assign(collaboration, data);
