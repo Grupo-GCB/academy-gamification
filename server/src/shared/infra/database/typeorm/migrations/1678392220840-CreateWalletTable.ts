ddf5c2164b6ecdf1cde95e484d9847711f466c5e
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-10 18:59:07 -0300

    feat: creates CreateWalletTable migration

--- server/src/shared/infra/database/typeorm/migrations/1678392220840-CreateWalletTable.ts
+++ server/src/shared/infra/database/typeorm/migrations/1678392220840-CreateWalletTable.ts
@@ -1,7 +1,37 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateWalletTable1678392220840 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'wallets',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'collaborator_id',
+            type: 'uuid',
+            isNullable: false,
+          },
+          {
+            name: 'gcbits',
+            type: 'numeric',
+            isNullable: false,
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            isNullable: true,
+          },
+        ],
+      }),
+    );
+  }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('wallets');
+  }
 }
