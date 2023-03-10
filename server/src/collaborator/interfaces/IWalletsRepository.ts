d0ee8f24409c7024b6d7a5c17e19509aa577bcd7
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-10 17:12:41 -0300

    feat: creates method create

--- server/src/collaborator/interfaces/IWalletsRepository.ts
+++ server/src/collaborator/interfaces/IWalletsRepository.ts
@@ -1,7 +1,8 @@
-import { UpdateGcbitsDTO } from '@collaborator/dto';
+import { UpdateGcbitsDTO, RegisterWalletDTO } from '@collaborator/dto';
 import { Wallet } from '@collaborator/infra/entities/wallet.entity';
 
 export abstract class IWalletsRepository {
   abstract findOne(id: string): Promise<Wallet>;
   abstract updateGcbits({ id, gcbits }: UpdateGcbitsDTO): Promise<Wallet>;
+  abstract create(data: RegisterWalletDTO): Promise<Wallet>;
 }
