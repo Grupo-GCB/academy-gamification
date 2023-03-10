8d05d75f59d6ad8c45968239ac68a7ef1e13b3a8
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-10 17:05:44 -0300

    feat: sets providers

--- server/src/collaborator/collaborator.module.ts
+++ server/src/collaborator/collaborator.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
-import { CollaboratorController } from './collaborator.controller';
-import { CollaboratorService } from './use-cases/collaborator.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CollaboratorsController } from '@shared/infra/http/controllers/collaborators.controller';
+import { Collaborator } from '@collaborator/infra/entities/collaborator.entity';
+import { RegisterCollaborator, UpdateGcbits } from './use-cases';
+import { CreateWallet } from './use-cases/createWallet/create-wallet';
+import { ICollaboratorsRepository } from './interfaces/ICollaboratorRepository';
+import { CollaboratorsRepository } from './infra/repositories/collaborators.repository';
+import { IWalletsRepository } from './interfaces';
+import { WalletsRepository } from './infra/repositories/wallets.repository';
 
 @Module({
-  controllers: [CollaboratorController],
-  providers: [CollaboratorService],
+  imports: [TypeOrmModule.forFeature([Collaborator])],
+  controllers: [CollaboratorsController],
+  providers: [
+    RegisterCollaborator,
+    UpdateGcbits,
+    CreateWallet,
+    {
+      provide: ICollaboratorsRepository,
+      useClass: CollaboratorsRepository,
+    },
+    {
+      provide: IWalletsRepository,
+      useClass: WalletsRepository,
+    },
+  ],
 })
 export class CollaboratorModule {}
