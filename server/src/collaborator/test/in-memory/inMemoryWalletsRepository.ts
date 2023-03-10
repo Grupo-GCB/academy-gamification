910053db7d3da297cb3737f1f07899cfb25bc802
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-10 17:13:13 -0300

    feat: implements create inside inMemoryWalletRepository

--- server/src/collaborator/test/in-memory/inMemoryWalletsRepository.ts
+++ server/src/collaborator/test/in-memory/inMemoryWalletsRepository.ts
@@ -1,9 +1,17 @@
-import { UpdateGcbitsDTO } from '@wallet/dto';
-import { Wallet } from '@wallet/infra/typeorm/entities/wallet.entity';
-import { IWalletsRepository } from '@wallet/interfaces';
+import { UpdateGcbitsDTO, RegisterWalletDTO } from '@collaborator/dto';
+import { Wallet } from '@collaborator/infra/entities/wallet.entity';
+import { IWalletsRepository } from '@collaborator/interfaces';
 
 export class InMemoryWalletsRepository implements IWalletsRepository {
   wallets: Wallet[] = [];
+
+  async create(data: RegisterWalletDTO): Promise<Wallet> {
+    const wallet: Wallet = Object.assign(new Wallet(), data);
+
+    this.wallets.push(wallet);
+
+    return wallet;
+  }
 
   async findOne(id: string): Promise<Wallet> {
     return this.wallets.find((wallets) => wallets.id === id);
