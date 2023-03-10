afa1d0f1fbdf665a5a353152992568a287b282ce
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-10 18:58:32 -0300

    feat: creates CreateCollaboratorTable

---
+++ server/src/shared/infra/database/typeorm/migrations/1678219006006-CreateCollaboratorTable.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCollaboratorTable1678219006006
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'collaborators',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('collaborators');
+  }
+}
