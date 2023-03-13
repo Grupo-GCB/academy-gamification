dc7dede784fa6bea77bf4023e6d634b56bedf1be
GuWuelta <rm93449@fiap.com.br>
2023-03-13 18:45:47 -0300

    feat: creates user module

---
+++ server/src/users/user.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { UsersController } from '@shared/infra/http/controllers/users.controller';
+import { User } from '@users/infra/entities/user.entity';
+import { UsersRepository } from '@users/infra/repositories/users.repository';
+import { IUsersRepository } from '@users/interfaces/IUsersRepository';
+import { RegisterUser } from '@users/use-cases/registerUser/register-user';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [
+    RegisterUser,
+    {
+      provide: IUsersRepository,
+      useClass: UsersRepository,
+    },
+  ],
+})
+export class UsersModule {}
