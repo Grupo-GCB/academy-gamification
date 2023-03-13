1f263d3cf040a6c466c121f13a7f7ea4e850b2ed
GuWuelta <rm93449@fiap.com.br>
2023-03-13 18:45:27 -0300

    test: users controller

---
+++ server/src/shared/infra/http/controllers/users.e2e.spec.ts
@@ -0,0 +1,45 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserRoles } from '@shared/constants';
+import request from 'supertest';
+
+import { AppModule } from '@/app.module';
+import { RegisterUser } from '@users/use-cases/registerUser/register-user';
+
+describe('Users Controller', () => {
+  const registerUser = {
+    execute: () => ({
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      passowrd: 'johndoe123',
+      role: UserRoles.ADMIN,
+    }),
+  };
+
+  let app: INestApplication;
+  let moduleRef: TestingModule;
+  beforeAll(async () => {
+    moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideProvider(RegisterUser)
+      .useValue(registerUser)
+      .compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('Register an user', () => {
+    it('should be able to return a 201 status if create user', () => {
+      return request(app.getHttpServer())
+        .post('/users')
+        .expect(201)
+        .expect(registerUser.execute());
+    });
+  });
+});
