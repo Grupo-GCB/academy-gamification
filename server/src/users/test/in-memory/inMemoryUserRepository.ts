8f95e71caf9e939961af44edf67c4842d0c5c3b6
GuWuelta <rm93449@fiap.com.br>
2023-03-13 18:47:10 -0300

    feat: creates in memory users repository

---
+++ server/src/users/test/in-memory/inMemoryUserRepository.ts
@@ -0,0 +1,19 @@
+import { RegisterUserDTO } from '@users/dto/register-user-dto';
+import { User } from '@users/infra/entities/user.entity';
+import { IUsersRepository } from '@users/interfaces/IUsersRepository';
+
+export class InMemoryUsersRepository implements IUsersRepository {
+  users: User[] = [];
+
+  async create(data: RegisterUserDTO): Promise<User> {
+    const user: User = Object.assign(new User(), data);
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  async findOne(id: string): Promise<User> {
+    return this.users.find((user) => user.id === id);
+  }
+}
