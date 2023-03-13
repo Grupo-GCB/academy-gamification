6ae72e9f4d4911b866819c6429c22d7542bed17d
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-13 18:38:48 -0300

    chore: adds swagger documentation

--- server/src/reward/dto/create-reward.dto.ts
+++ server/src/reward/dto/create-reward.dto.ts
@@ -1,14 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 export class CreateRewardDTO {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'Créditor PeerBr',
+    description: 'Título da recompensa',
+    type: 'string',
+    required: true,
+  })
   name: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'Créditos a serem utilizados dentro do PeerBr',
+    description: 'Descrição informando mais detalhes sobre a recompensa',
+    type: 'string',
+    required: true,
+  })
   description: string;
 
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty({
+    example: '5000',
+    description: 'Valor para recompensa ser resgatada',
+    type: 'number',
+    required: true,
+  })
   value: number;
 }
