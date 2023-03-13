df4e128c0481a342861c7e34a3a81306f17702a2
GuWuelta <rm93449@fiap.com.br>
2023-03-13 18:47:47 -0300

    test: register user use case

---
+++ server/src/users/use-cases/registerUser/register-user.spec.ts
@@ -0,0 +1,31 @@
+import { UserRoles } from '@shared/constants';
+import { InMemoryUsersRepository } from '@users/test/in-memory/inMemoryUserRepository';
+import { RegisterUser } from './register-user';
+
+describe('Register collaborator', () => {
+  let inMemoryUsersRepository: InMemoryUsersRepository;
+  let sut: RegisterUser;
+
+  beforeAll(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    sut = new RegisterUser(inMemoryUsersRepository);
+  });
+
+  it('should be able to register an user', async () => {
+    const user = await sut.execute({
+      name: 'Gustavo',
+      email: 'gustavo.wuelta@gcbinvestimentos.com',
+      password: 'gcb123',
+      role: UserRoles.ACADEMY,
+    });
+
+    expect(user).toEqual(
+      expect.objectContaining({
+        name: 'Gustavo',
+        email: 'gustavo.wuelta@gcbinvestimentos.com',
+        password: 'gcb123',
+        role: UserRoles.ACADEMY,
+      }),
+    );
+  });
+});
