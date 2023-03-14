bbe79ecc631a51e603776c39e4b3e68de518ba31
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-13 23:23:25 -0300

    adds reward module to app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TransactionsModule } from '@transactions/transactions.module';
 
 import { CollaborationsModule } from './collaborations/collaborations.module';
+import { RewardsModule } from '@reward/reward.module';
+import { TransactionsModule } from '@transactions/transactions.module';
+
 import { ormconfig } from './ormconfig';
 
 @Module({
@@ -10,6 +12,7 @@ import { ormconfig } from './ormconfig';
     TypeOrmModule.forRoot(ormconfig),
     CollaborationsModule,
     TransactionsModule,
+    RewardsModule,
   ],
 })
 export class AppModule {}
