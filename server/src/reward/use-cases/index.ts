cfd3bc42c196dedb3f1817e2cae42b82e60e3048
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-14 10:44:38 -0300

    chore: exports deleteReward

--- server/src/reward/use-cases/index.ts
+++ server/src/reward/use-cases/index.ts
@@ -2,3 +2,4 @@ export * from '@reward/use-cases/createReward/create-reward';
 export * from '@reward/use-cases/listAll/list-all-rewards';
 export * from '@reward/use-cases/findOne/find-one-reward';
 export * from '@reward/use-cases/updateReward/update-reward';
+export * from '@reward/use-cases/deleteReward/delete-reward';
