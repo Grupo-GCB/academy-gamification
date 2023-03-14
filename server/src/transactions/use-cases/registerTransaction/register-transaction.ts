c70c74614665b35271807f32a7813cc85ee5dac1
GuWuelta <rm93449@fiap.com.br>
2023-03-14 20:00:46 -0300

    refactor: register transaction use case

--- server/src/transactions/use-cases/registerTransaction/register-transaction.ts
+++ server/src/transactions/use-cases/registerTransaction/register-transaction.ts
@@ -1,11 +1,10 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 
-import { Roles } from '@shared/constants';
+import { Reasons, Roles } from '@shared/constants';
 import { RegisterTransactionDTO } from '@transactions/dto';
 import { Transaction } from '@transactions/infra/typeorm/entities/transaction.entity';
 import { ITransactionsRepository } from '@transactions/interfaces';
 import { IUsersRepository } from '@users/interfaces/IUsersRepository';
-import { FindByEmail } from '@users/use-cases/findByEmail/find-by-email';
 
 @Injectable()
 export class RegisterTransaction {
@@ -15,9 +14,18 @@ export class RegisterTransaction {
   ) {}
 
   async execute(data: RegisterTransactionDTO): Promise<Transaction> {
-    const findByEmail = new FindByEmail(this.usersRepository);
-    const responsible = findByEmail.execute(data.responsible);
-    if ((await responsible).role == Roles.COLLABORATOR) {
+    const responsible = await this.usersRepository.findOne(data.responsible);
+
+    if (
+      responsible.role == Roles.COLLABORATOR &&
+      data.reason != Reasons.REDEEM
+    ) {
+      throw new UnauthorizedException('You do not have permission');
+    }
+    if (
+      responsible.role == Roles.ACADEMY &&
+      data.reason != Reasons.COLLABORATION
+    ) {
       throw new UnauthorizedException('You do not have permission');
     }
     return this.transactionsRepository.register(data);
