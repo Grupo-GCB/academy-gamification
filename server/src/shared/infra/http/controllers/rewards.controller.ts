8c035912513f894581a09c4e04913531957ad535
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-13 23:44:13 -0300

    feat: implements findAll on controller

--- server/src/shared/infra/http/controllers/rewards.controller.ts
+++ server/src/shared/infra/http/controllers/rewards.controller.ts
@@ -1,13 +1,23 @@
-import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
-import { ApiBadRequestResponse, ApiCreatedResponse } from '@nestjs/swagger';
+import { Body, Controller, Get, HttpStatus, Post } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+
 import { CreateRewardDTO } from '@reward/dto';
 import { Reward } from '@reward/infra/entities/reward.entity';
+import { CreateReward, ListAllRewards } from '@reward/use-cases';
 
-import { CreateReward } from '@reward/use-cases';
-
+@ApiTags('rewards')
 @Controller('rewards')
 export class RewardsController {
-  constructor(private createReward: CreateReward) {}
+  constructor(
+    private createReward: CreateReward,
+    private listAllRewards: ListAllRewards,
+  ) {}
 
   @ApiCreatedResponse({
     status: HttpStatus.CREATED,
@@ -20,5 +30,17 @@ export class RewardsController {
   @Post()
   create(@Body() data: CreateRewardDTO): Promise<Reward> {
     return this.createReward.execute(data);
+  }
+
+  @ApiOkResponse({
+    status: HttpStatus.OK,
+  })
+  @ApiNotFoundResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Não foi possível encontrar recompensas',
+  })
+  @Get()
+  listAll(): Promise<Reward[]> {
+    return this.listAllRewards.execute();
   }
 }
