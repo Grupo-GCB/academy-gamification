b82ff32deb9fc0e8feec7108791844f3fca45f7d
GuWuelta <rm93449@fiap.com.br>
2023-03-14 00:44:44 -0300

    chore: removes unecessary dto

--- server/src/collaborations/dto/index.ts
+++ server/src/collaborations/dto/index.ts
@@ -1,4 +1,3 @@
 export * from './filter-collaborations-by-status';
 export * from './register-collaboration.dto';
-export * from './update-collaboration.dto';
 export * from './update-status.dto';
--- server/src/collaborations/dto/update-collaboration.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-
-import { RegisterCollaborationDTO } from './register-collaboration.dto';
-
-export class UpdateCollaborationDto extends PartialType(
-  RegisterCollaborationDTO,
-) {}
