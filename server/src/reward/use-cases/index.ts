a1061ba98638da9c7d84a49fa86c59f60b09b9a6
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-14 08:37:55 -0300

    feat: exports update reward

--- server/src/reward/use-cases/index.ts
+++ server/src/reward/use-cases/index.ts
@@ -1,3 +1,4 @@
 export * from '@reward/use-cases/createReward/create-reward';
 export * from '@reward/use-cases/listAll/list-all-rewards';
 export * from '@reward/use-cases/findOne/find-one-reward';
+export * from '@reward/use-cases/updateReward/update-reward';
