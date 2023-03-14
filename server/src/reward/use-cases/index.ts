06326567ba22a75dad0e1d2b426db89b116ef16c
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-14 00:30:04 -0300

    chore: exports findOne

--- server/src/reward/use-cases/index.ts
+++ server/src/reward/use-cases/index.ts
@@ -1,2 +1,3 @@
 export * from '@reward/use-cases/createReward/create-reward';
 export * from '@reward/use-cases/listAll/list-all-rewards';
+export * from '@reward/use-cases/findOne/find-one-reward';
