5a123ab8ad437efa1a1b7f28022ef96804e0dbe6
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-14 08:25:48 -0300

    feat: creates updatable reward dto

---
+++ server/src/reward/dto/updatable-reward-data.dto.ts
@@ -0,0 +1,44 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsNumber, IsString } from 'class-validator';
+
+export class UpdatableRewardDataDTO {
+  @IsString()
+  @IsOptional()
+  @ApiProperty({
+    example: 'Créditos PeerBr',
+    description: 'Título da recompensa',
+    type: 'string',
+    required: true,
+  })
+  name?: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({
+    example: 'Créditos a serem utilizados dentro do PeerBr',
+    description: 'Descrição informando mais detalhes sobre a recompensa',
+    type: 'string',
+    required: false,
+  })
+  description?: string;
+
+  @IsNumber()
+  @IsOptional()
+  @ApiProperty({
+    example: '5000',
+    description: 'Valor para recompensa ser resgatada',
+    type: 'number',
+    required: false,
+  })
+  value?: number;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({
+    example: 'https://cdn.maikoapp.com/3d4b/4quqa/150.jpg',
+    description: 'Lind da imagem da recompensa',
+    type: 'string',
+    required: false,
+  })
+  imageUrl?: string;
+}
