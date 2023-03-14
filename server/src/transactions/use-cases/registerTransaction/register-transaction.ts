b33428532b962ef3f6507c2860d410f35b856b95
GuWuelta <rm93449@fiap.com.br>
2023-03-14 00:48:31 -0300

    feat: implements user role validation

--- server/src/transactions/use-cases/registerTransaction/register-transaction.ts
+++ server/src/transactions/use-cases/registerTransaction/register-transaction.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@nestjs/common';
 
-import { ITransactionsRepository } from '@transactions/interfaces';
+import { UserRoles } from '@shared/constants';
 import { RegisterTransactionDTO } from '@transactions/dto';
 import { Transaction } from '@transactions/infra/typeorm/entities/transaction.entity';
+import { ITransactionsRepository } from '@transactions/interfaces';
+import { IUsersRepository } from '@users/interfaces/IUsersRepository';
+import { FindById } from '@users/use-cases/findById/find-by-id';
 
 @Injectable()
 export class RegisterTransaction {
-  constructor(private transactionsRepository: ITransactionsRepository) {}
+  constructor(
+    private transactionsRepository: ITransactionsRepository,
+    private usersRepository: IUsersRepository,
+  ) {}
 
   async execute(data: RegisterTransactionDTO): Promise<Transaction> {
+    const findById = new FindById(this.usersRepository);
+    const user = findById.execute(data.user_id);
+    if ((await user).role == UserRoles.COLLABORATOR) {
+      throw new Error('Collaborators are not able to record transactions');
+    }
     return this.transactionsRepository.register(data);
   }
 }
