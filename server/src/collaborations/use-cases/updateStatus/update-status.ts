53c792e7428042b9db667ef0b607184f74b28949
GuWuelta <rm93449@fiap.com.br>
2023-03-14 00:46:23 -0300

    feat: implements user role validation

--- server/src/collaborations/use-cases/updateStatus/update-status.ts
+++ server/src/collaborations/use-cases/updateStatus/update-status.ts
@@ -7,16 +7,28 @@ import {
 import { UpdateStatusDTO } from '@collaborations/dto';
 import { Collaboration } from '@collaborations/infra/typeorm/entities/collaboration.entity';
 import { ICollaborationsRepository } from '@collaborations/interfaces';
+import { UserRoles } from '@shared/constants';
+import { IUsersRepository } from '@users/interfaces/IUsersRepository';
+import { FindById } from '@users/use-cases/findById/find-by-id';
 
 @Injectable()
 export class UpdateStatus {
-  constructor(private collaborationsRepository: ICollaborationsRepository) {}
+  constructor(
+    private collaborationsRepository: ICollaborationsRepository,
+    private usersRepository: IUsersRepository,
+  ) {}
 
   async execute({
     collaboration_id,
     newStatus,
+    admin_id,
   }: UpdateStatusDTO): Promise<Collaboration> {
     if (!newStatus) throw new BadRequestException('newStatus is required');
+    const findById = new FindById(this.usersRepository);
+    const user = findById.execute(admin_id);
+    if ((await user).role != UserRoles.ADMIN) {
+      throw new Error('You should must be a administrator');
+    }
 
     const collaboration: Collaboration =
       await this.collaborationsRepository.findOne(collaboration_id);
@@ -28,6 +40,7 @@ export class UpdateStatus {
     return this.collaborationsRepository.updateStatus({
       collaboration_id,
       newStatus,
+      admin_id,
     });
   }
 }
