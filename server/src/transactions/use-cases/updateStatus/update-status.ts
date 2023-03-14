88c16e0e6ab4b9dff9681b95a399705f297dfec2
GuWuelta <rm93449@fiap.com.br>
2023-03-14 00:48:52 -0300

    feat: implements user role validation

--- server/src/transactions/use-cases/updateStatus/update-status.ts
+++ server/src/transactions/use-cases/updateStatus/update-status.ts
@@ -3,18 +3,34 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
+import { UserRoles } from '@shared/constants';
 
 import { UpdateStatusDTO } from '@transactions/dto';
 import { Transaction } from '@transactions/infra/typeorm/entities/transaction.entity';
 import { ITransactionsRepository } from '@transactions/interfaces';
+import { IUsersRepository } from '@users/interfaces/IUsersRepository';
+import { FindById } from '@users/use-cases/findById/find-by-id';
 
 @Injectable()
 export class UpdateStatus {
-  constructor(private transactionsRepository: ITransactionsRepository) {}
+  constructor(
+    private transactionsRepository: ITransactionsRepository,
+    private usersRepository: IUsersRepository,
+  ) {}
 
-  async execute({ id, newStatus }: UpdateStatusDTO): Promise<Transaction> {
+  async execute({
+    id,
+    newStatus,
+    user_id,
+  }: UpdateStatusDTO): Promise<Transaction> {
     if (!newStatus) throw new BadRequestException('newStatus is required');
     if (!id) throw new BadRequestException('id is required');
+
+    const findById = new FindById(this.usersRepository);
+    const user = findById.execute(user_id);
+    if ((await user).role != UserRoles.ADMIN) {
+      throw new Error('You should must be a administrator');
+    }
 
     const transaction: Transaction = await this.transactionsRepository.findOne(
       id,
@@ -27,6 +43,7 @@ export class UpdateStatus {
     return this.transactionsRepository.updateStatus({
       id,
       newStatus,
+      user_id,
     });
   }
 }
