c535d16e2a9a14b72a2ff8aa5e3fe87190395b09
GuWuelta <rm93449@fiap.com.br>
2023-03-15 18:17:50 -0300

    test: register use case
    Co-authored-by: Vitor Mantovani <VitorMantovani@users.noreply.github.com>

--- server/src/users/use-cases/registerUser/register-user.spec.ts
+++ server/src/users/use-cases/registerUser/register-user.spec.ts
@@ -2,7 +2,7 @@ import { Roles } from '@shared/constants';
 import { InMemoryUsersRepository } from '@users/test/in-memory/inMemoryUserRepository';
 import { RegisterUser } from './register-user';
 
-describe('Register collaborator', () => {
+describe('Register user', () => {
   let inMemoryUsersRepository: InMemoryUsersRepository;
   let sut: RegisterUser;
 
@@ -25,6 +25,42 @@ describe('Register collaborator', () => {
         email: 'gustavo.wuelta@gcbinvestimentos.com',
         password: 'gcb123',
         role: Roles.ACADEMY,
+      }),
+    );
+  });
+
+  it('should be able to register an with Admin role', async () => {
+    const user = await sut.execute({
+      name: 'Gustavo',
+      email: 'gustavo.wuelta@gcbinvestimentos.com',
+      password: 'gcb123',
+      role: Roles.ADMIN,
+    });
+
+    expect(user).toEqual(
+      expect.objectContaining({
+        name: 'Gustavo',
+        email: 'gustavo.wuelta@gcbinvestimentos.com',
+        password: 'gcb123',
+        role: Roles.ADMIN,
+      }),
+    );
+  });
+
+  it('should be able to register an user with Collaborator role', async () => {
+    const user = await sut.execute({
+      name: 'Gustavo',
+      email: 'gustavo.wuelta@gcbinvestimentos.com',
+      password: 'gcb123',
+      role: Roles.COLLABORATOR,
+    });
+
+    expect(user).toEqual(
+      expect.objectContaining({
+        name: 'Gustavo',
+        email: 'gustavo.wuelta@gcbinvestimentos.com',
+        password: 'gcb123',
+        role: Roles.COLLABORATOR,
       }),
     );
   });
