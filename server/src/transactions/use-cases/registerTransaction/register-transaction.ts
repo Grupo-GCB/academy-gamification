fd937f5af645734f51d629f234096b8cc745450b
GuWuelta <rm93449@fiap.com.br>
2023-03-15 18:19:51 -0300

    feat: creates register transaction use case

--- server/src/transactions/use-cases/registerTransaction/register-transaction.ts
+++ server/src/transactions/use-cases/registerTransaction/register-transaction.ts
@@ -13,21 +13,34 @@ export class RegisterTransaction {
     private usersRepository: IUsersRepository,
   ) {}
 
-  async execute(data: RegisterTransactionDTO): Promise<Transaction> {
-    const responsible = await this.usersRepository.findOne(data.responsible);
+  async execute({
+    collaborator,
+    responsible,
+    reason,
+    type,
+    business_unit,
+    status,
+    gcbits,
+    description,
+  }: RegisterTransactionDTO): Promise<Transaction> {
+    const responsiblee = await this.usersRepository.findOne(responsible);
 
     if (
-      responsible.role == Roles.COLLABORATOR &&
-      data.reason != Reasons.REEDEM
+      (responsiblee.role == Roles.COLLABORATOR && reason != Reasons.REDEEM) ||
+      (responsiblee.role == Roles.ACADEMY && reason != Reasons.COLLABORATION)
     ) {
       throw new UnauthorizedException('You do not have permission');
     }
-    if (
-      responsible.role == Roles.ACADEMY &&
-      data.reason != Reasons.COLLABORATION
-    ) {
-      throw new UnauthorizedException('You do not have permission');
-    }
-    return this.transactionsRepository.register(data);
+
+    return this.transactionsRepository.register({
+      collaborator,
+      responsible,
+      reason,
+      type,
+      business_unit,
+      status,
+      gcbits,
+      description,
+    });
   }
 }
