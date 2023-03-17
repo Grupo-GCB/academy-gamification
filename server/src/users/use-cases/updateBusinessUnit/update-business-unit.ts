7f2dcb77cc6af1384fc3ecc0f2c6718ad5fa88af
GuWuelta <rm93449@fiap.com.br>
2023-03-17 02:48:25 -0300

    feat: creates update business unit use case

---
+++ server/src/users/use-cases/updateBusinessUnit/update-business-unit.ts
@@ -0,0 +1,46 @@
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+import { Roles } from '@shared/constants';
+import { UpdateBusinessUnitDTO } from '@users/dto';
+import { User } from '@users/infra/entities/user.entity';
+import { IUsersRepository } from '@users/interfaces/IUsersRepository';
+
+@Injectable()
+export class UpdateBusinessUnit {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  async execute({
+    id,
+    responsible,
+    new_bu,
+  }: UpdateBusinessUnitDTO): Promise<User> {
+    if (!new_bu) throw new BadRequestException('Business unit is required');
+    if (!id) throw new BadRequestException('User id is required');
+    if (!responsible) throw new BadRequestException('Responsible is required');
+
+    const user = await this.usersRepository.findOne(id);
+    const updateResponsible = await this.usersRepository.findOne(responsible);
+
+    if (!user || !updateResponsible) {
+      throw new BadRequestException('User or responsible does not exist');
+    }
+
+    if (updateResponsible.role == Roles.ACADEMY) {
+      throw new UnauthorizedException('Academys cannot perform this action');
+    }
+
+    if (user.business_unit === new_bu) {
+      throw new BadRequestException('User already in this business unit');
+    }
+
+    return this.usersRepository.updateStatus({
+      id,
+      responsible,
+      new_bu,
+    });
+  }
+}
