a5e8bc92320aeb472bef18c65bfa80b990088912
GuWuelta <rm93449@fiap.com.br>
2023-03-21 16:55:41 -0300

    feat: creates update password use case

---
+++ server/src/users/use-cases/updatePassword/update-password.ts
@@ -0,0 +1,37 @@
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { hash } from 'bcrypt';
+
+import { UpdatePasswordDTO } from '@users/dto/update-password.dto';
+import { User } from '@users/infra/entities/user.entity';
+import { IUsersRepository } from '@users/interfaces/IUsersRepository';
+
+Injectable();
+export class UpdatePassword {
+  constructor(
+    @Inject(IUsersRepository)
+    private readonly usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({
+    id,
+    password,
+    new_password,
+  }: UpdatePasswordDTO): Promise<User> {
+    const user = await this.usersRepository.findOne(id);
+
+    if (!user) {
+      throw new BadRequestException('User does not exist');
+    }
+
+    if (password !== user.password) {
+      throw new BadRequestException('Incorrect current password');
+    }
+
+    new_password = await hash(new_password, 8);
+
+    return this.usersRepository.updatePassword({
+      id,
+      new_password,
+    });
+  }
+}
