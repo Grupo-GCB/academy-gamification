2249e1b90493105ae5bc3e5a38c5af9d666d6064
GuWuelta <rm93449@fiap.com.br>
2023-03-21 16:54:52 -0300

    feat: adds update password export

--- server/src/users/use-cases/index.ts
+++ server/src/users/use-cases/index.ts
@@ -2,3 +2,4 @@ export * from './deleteUser/delete-user';
 export * from './findById/find-by-id';
 export * from './registerUser/register-user';
 export * from './updateBusinessUnit/update-business-unit';
+export * from './updatePassword/update-password';
