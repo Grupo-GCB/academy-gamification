f2c4d2cb3e904f8cc090b8f5404aec52238026aa
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-21 10:39:16 -0300

    feat: adds AuthModule to imports

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { TransactionsModule } from '@transactions/transactions.module';
 import { UsersModule } from '@users/user.module';
+import { AuthModule } from './auth/auth.module';
 import { ormconfig } from './ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), TransactionsModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot(ormconfig),
+    TransactionsModule,
+    UsersModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
