c9a7b99b1e97b89cad1d33eabf4fb8c080848d6e
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-22 16:18:05 -0300

    feat: creates refresh token entity

---
+++ server/src/auth/infra/typeorm/entities/refresh-token.entity.ts
@@ -0,0 +1,37 @@
+import { User } from '@users/infra/entities/user.entity';
+import { randomUUID } from 'node:crypto';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from 'typeorm';
+
+@Entity('refresh_tokens')
+export class RefreshToken {
+  @PrimaryColumn()
+  id: string;
+
+  @Column({ unique: true })
+  token: string;
+
+  @ManyToOne(() => User, (user) => user.id)
+  @JoinColumn({
+    name: 'user',
+    referencedColumnName: 'id',
+  })
+  @Column()
+  user: string;
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  expiresAt?: Date;
+
+  constructor() {
+    if (!this.id) this.id = randomUUID();
+  }
+}
