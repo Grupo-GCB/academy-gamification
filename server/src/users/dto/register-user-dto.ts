462bb8b4337ddf2d755efceaa433aac89a8010dc
GuWuelta <meypo.gustavo@gmail.com>
2023-03-22 19:24:04 -0300

    chore: removes name, password and role props

--- server/src/users/dto/register-user-dto.ts
+++ server/src/users/dto/register-user-dto.ts
@@ -1,19 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 
-import { BusinessUnits, Roles } from '@shared/constants';
+import { BusinessUnits } from '@shared/constants';
 
 export class RegisterUserDTO {
-  @IsNotEmpty()
-  @IsString()
-  @ApiProperty({
-    example: 'John Doe',
-    description: 'Nome do usuário',
-    type: 'string',
-    required: true,
-  })
-  name: string;
-
   @IsString()
   @IsNotEmpty()
   @ApiProperty({
@@ -24,16 +14,6 @@ export class RegisterUserDTO {
   })
   email: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @ApiProperty({
-    example: 'gcb123',
-    description: 'Senha do usuário',
-    type: 'string',
-    required: true,
-  })
-  password: string;
-
   @IsNotEmpty()
   @IsEnum(BusinessUnits)
   @ApiProperty({
@@ -43,14 +23,4 @@ export class RegisterUserDTO {
     required: true,
   })
   business_unit: BusinessUnits;
-
-  @IsNotEmpty()
-  @IsEnum(Roles)
-  @ApiProperty({
-    example: 'ADMIN',
-    description: 'Cargo de atuação do usuário',
-    type: 'Roles',
-    required: true,
-  })
-  role: Roles;
 }
