07d47dbdf09d0fb676f0d4ffad0fd07341125ff4
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-22 16:14:12 -0300

    chore: fixes auth.module import

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { TransactionsModule } from '@transactions/transactions.module';
 import { UsersModule } from '@users/user.module';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from '@auth/auth.module';
 import { ormconfig } from './ormconfig';
 
 @Module({
