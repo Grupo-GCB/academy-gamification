cf22cfd17d9a6cf13915fedad4f733e193c97167
GuWuelta <meypo.gustavo@gmail.com>
2023-03-22 19:25:41 -0300

    feat: adds encrypt password method

--- server/src/users/test/in-memory/inMemoryUserRepository.ts
+++ server/src/users/test/in-memory/inMemoryUserRepository.ts
@@ -1,12 +1,16 @@
-import { RegisterUserDTO, UpdateBusinessUnitDTO } from '@users/dto';
+import { UpdateBusinessUnitDTO } from '@users/dto';
 import { User } from '@users/infra/entities/user.entity';
+import { IRegisterUser } from '@users/interfaces/IRegisterUser';
 import { IUpdatePassword } from '@users/interfaces/IUpdatePassword';
 import { IUsersRepository } from '@users/interfaces/IUsersRepository';
+import { hash } from 'bcrypt';
 
 export class InMemoryUsersRepository implements IUsersRepository {
   users: User[] = [];
 
-  async create(data: RegisterUserDTO): Promise<User> {
+  async create(data: IRegisterUser): Promise<User> {
+    data.password = await hash(data.password, 8);
+
     const user: User = Object.assign(new User(), data);
 
     this.users.push(user);
@@ -35,11 +39,9 @@ export class InMemoryUsersRepository implements IUsersRepository {
     user.deleted_at = new Date();
   }
 
-  async updatePassword({ id, new_password }: IUpdatePassword): Promise<User> {
+  async updatePassword({ id, new_password }: IUpdatePassword): Promise<void> {
     const user = await this.findOne(id);
 
     user.password = new_password;
-
-    return user;
   }
 }
