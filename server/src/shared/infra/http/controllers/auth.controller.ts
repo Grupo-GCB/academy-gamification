64d1ba0b21bb44e489b8a7f9e63ad2b484a090cc
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-22 16:21:00 -0300

    feat: implements refresh and logout to controller

--- server/src/shared/infra/http/controllers/auth.controller.ts
+++ server/src/shared/infra/http/controllers/auth.controller.ts
@@ -5,12 +5,15 @@ import {
   Post,
   UseGuards,
   Request,
+  Body,
 } from '@nestjs/common';
 
 import { AuthService } from '@auth/auth.service';
 import { LocalAuthGuard } from '@auth/guards';
 import { IAuthRequest } from '@auth/interfaces';
 import { IsPublic } from '@auth/decorators';
+import { RefreshToken } from '@auth/infra/typeorm/entities/refresh-token.entity';
+import { RefreshTokenGuard } from '@auth/guards/refresh-token.guard';
 
 @Controller()
 export class AuthController {
@@ -22,5 +25,21 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   login(@Request() req: IAuthRequest) {
     return this.authService.login(req.user);
+  }
+
+  @Post('refresh')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(RefreshTokenGuard)
+  async refresh(@Body() refreshToken: string) {
+    return this.authService.refresh(refreshToken);
+  }
+
+  @Post('logout')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @UseGuards(RefreshTokenGuard)
+  async logout(@Request() req) {
+    const refreshToken = req.body.refreshToken;
+    await this.authService.logout(refreshToken);
+    return;
   }
 }
