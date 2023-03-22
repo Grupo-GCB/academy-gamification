d0f69ff8e0614bb45cfbe371ed58cbb69669b729
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-22 16:17:20 -0300

    feat: create refres token guard

---
+++ server/src/auth/guards/refresh-token.guard.ts
@@ -0,0 +1,28 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthService } from '@auth/auth.service';
+
+@Injectable()
+export class RefreshTokenGuard implements CanActivate {
+  constructor(private authService: AuthService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req = context.switchToHttp().getRequest();
+
+    const refreshToken = req.body.refreshToken;
+    if (!refreshToken) {
+      throw new UnauthorizedException('Refresh token not provided');
+    }
+
+    const isValid = await this.authService.verifyRefreshToken(refreshToken);
+    if (!isValid) {
+      throw new UnauthorizedException('Invalid refresh token');
+    }
+
+    return true;
+  }
+}
