2f886c58a433cc41ae6c94104a30546e54ec873a
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-22 16:15:36 -0300

    feat: adds refresh token method

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { compare, bcrypt } from 'bcryptjs';
+import { bcrypt } from 'bcryptjs';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
@@ -6,23 +6,31 @@ import { FindByEmail } from '@users/use-cases';
 import { User } from '@users/infra/entities/user.entity';
 import { IJwtPayload, IUserToken } from '@auth/interfaces';
 import { LoginDTO } from '@auth/dto';
+import { RefreshTokenRepository } from '@auth/infra/typeorm/repositories/refresh-token.repository';
+import { RevokedTokenRepository } from '@auth/infra/typeorm/repositories/revoked-token.repository';
 
 @Injectable()
 export class AuthService {
   constructor(
     private findByEmail: FindByEmail,
     private jwtService: JwtService,
+    private refreshTokenRepository: RefreshTokenRepository,
+    private revokedTokenRepository: RevokedTokenRepository,
   ) {}
 
   async validateUser({ email, password }: LoginDTO): Promise<User> {
     const user = await this.findByEmail.execute(email);
 
-    if (user) {
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-      if (isPasswordValid) return { ...user, password: undefined };
+    // if (user) {
+    //   const isPasswordValid = await bcrypt.compare(password, user.password);
+    //   if (isPasswordValid) return { ...user, password: undefined };
+    // }
+
+    if (!user) {
+      throw new Error('Email address or password provided is incorrect.');
     }
 
-    throw new Error('Email address or password provided is incorrect.');
+    return user;
   }
 
   async login(user: User): Promise<IUserToken> {
@@ -33,33 +41,71 @@ export class AuthService {
       role: user.role,
     };
 
-    const accessToken = this.jwtService.sign(payload);
+    const accessToken = this.jwtService.sign(payload, {
+      expiresIn: '1m',
+    });
+
+    const refreshTokenExpiresIn = 2 * 24 * 60 * 60 * 1000;
+
+    const refreshToken = await this.refreshTokenRepository.createRefreshToken({
+      user: user.id,
+      expiresAt: refreshTokenExpiresIn,
+    });
 
     return {
       accessToken,
+      refreshToken: refreshToken.token,
     };
   }
 
-  // async refresh(refreshToken: string) {
-  //   const decoded = this.jwtService.decode(refreshToken) as IJwtPayload;
+  async refresh(token: string) {
+    const refreshToken = await this.refreshTokenRepository.findRefreshToken(
+      token,
+    );
 
-  //   const user = await this.validateUser(decoded);
+    console.log(refreshToken);
 
-  //   if (!user) throw new UnauthorizedException('Sem autorização');
+    if (!refreshToken) {
+      throw new UnauthorizedException('Refresh token inválido');
+    }
 
-  //   const payload: IJwtPayload = {
-  //     sub: user.id,
-  //     email: user.email,
-  //     bu: user.business_unit,
-  //     role: user.role,
-  //   };
+    if (new Date() > refreshToken.expiresAt) {
+      throw new UnauthorizedException('Refresh token expirado');
+    }
 
-  //   const newAccessToken = this.jwtService.sign(payload, {
-  //     expiresIn: '10m',
-  //   });
+    const user = await this.findByEmail.execute(refreshToken.user);
 
-  //   return {
-  //     accessToken: newAccessToken,
-  //   };
-  // }
+    if (!user) throw new UnauthorizedException('Sem autorização');
+
+    const payload: IJwtPayload = {
+      sub: user.id,
+      email: user.email,
+      bu: user.business_unit,
+      role: user.role,
+    };
+
+    const newAccessToken = this.jwtService.sign(payload, {
+      expiresIn: '10m',
+    });
+
+    return {
+      accessToken: newAccessToken,
+    };
+  }
+
+  async logout(token: string): Promise<void> {
+    try {
+      this.jwtService.verify(token);
+      await this.revokedTokenRepository.revokeToken(token);
+    } catch (error) {
+      throw new UnauthorizedException('Token inválido');
+    }
+  }
+
+  async verifyRefreshToken(token: string): Promise<boolean> {
+    const refreshToken = await this.refreshTokenRepository.findRefreshToken(
+      token,
+    );
+    return !!refreshToken;
+  }
 }
