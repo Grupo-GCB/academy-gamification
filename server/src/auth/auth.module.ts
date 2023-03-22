bf1345a0aca67da949936b4eb43ae3bd47b1e6c7
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-22 16:14:41 -0300

    feat: adds revoked and refresh token repository

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
@@ -7,6 +8,10 @@ import { AuthController } from '@shared/infra/http/controllers/auth.controller';
 import { UsersModule } from '@users/user.module';
 import { AuthService } from '@auth/auth.service';
 import { FindByEmail } from '@users/use-cases';
+import { RevokedTokenRepository } from '@auth/infra/typeorm/repositories/revoked-token.repository';
+import { RefreshTokenRepository } from '@auth/infra/typeorm/repositories/refresh-token.repository';
+import { RevokedToken } from './infra/typeorm/entities/revoked-token.entity';
+import { RefreshToken } from './infra/typeorm/entities/refresh-token.entity';
 
 @Module({
   imports: [
@@ -16,8 +21,16 @@ import { FindByEmail } from '@users/use-cases';
       secret: 'chave-secreta',
       signOptions: { expiresIn: '600s' },
     }),
+    TypeOrmModule.forFeature([RevokedToken, RefreshToken]),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, JwtStrategy, FindByEmail],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+    FindByEmail,
+    RefreshTokenRepository,
+    RevokedTokenRepository,
+  ],
 })
 export class AuthModule {}
