57a3d613933206e3b1d8e283e027bac3249dac92
GuWuelta <meypo.gustavo@gmail.com>
2023-03-23 17:38:43 -0300

    feat: adds SendGrid  import

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { SendGridModule } from '@anchan828/nest-sendgrid';
 import { TransactionsModule } from '@transactions/transactions.module';
 import { UsersModule } from '@users/user.module';
 import { ormconfig } from './ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), TransactionsModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot(ormconfig),
+    TransactionsModule,
+    UsersModule,
+    SendGridModule.forRoot({ apikey: process.env.SEND_GRID_ACESS_KEY }),
+  ],
 })
 export class AppModule {}
