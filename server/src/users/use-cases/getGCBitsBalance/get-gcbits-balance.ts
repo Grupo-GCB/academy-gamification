fe367ecf9988840824c0af1bb9c9da8add002a14
GuWuelta <meypo.gustavo@gmail.com>
2023-03-24 17:49:57 -0300

    feat: creates get gcbits balance use case

---
+++ server/src/users/use-cases/getGCBitsBalance/get-gcbits-balance.ts
@@ -0,0 +1,34 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Status } from '@shared/constants';
+import { FilterTransactionsByUserDTO } from '@transactions/dto/filter-transactions-by-user.dto';
+import { ITransactionsRepository } from '@transactions/interfaces';
+import { IGCBitsBalance, IUsersRepository } from '@users/interfaces';
+
+@Injectable()
+export class GetGCBitsBalance {
+  constructor(
+    private transactionsRepository: ITransactionsRepository,
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({
+    user,
+  }: FilterTransactionsByUserDTO): Promise<IGCBitsBalance> {
+    const fullUser = await this.usersRepository.findById(user);
+
+    if (!fullUser) {
+      throw new BadRequestException('User not found');
+    }
+
+    const transactions = await this.transactionsRepository.filterByUser({
+      user,
+    });
+
+    const balance = transactions.reduce((total, transaction) => {
+      if (transaction.status === Status.APPROVED) total += transaction.gcbits;
+      return total;
+    }, 0);
+
+    return { balance };
+  }
+}
