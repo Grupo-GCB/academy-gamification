b80a3418d52ba4ac6763eef4634338ed3aee061a
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-26 22:27:20 -0300

    feat: adds findById dependencie

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -7,7 +7,7 @@ import { JwtStrategy, LocalStrategy } from '@auth/strategies';
 import { AuthController } from '@shared/infra/http/controllers/auth.controller';
 import { UsersModule } from '@users/user.module';
 import { AuthService } from '@auth/auth.service';
-import { FindByEmail } from '@users/use-cases';
+import { FindByEmail, FindById } from '@users/use-cases';
 import { RevokedTokenRepository } from '@auth/infra/typeorm/repositories/revoked-token.repository';
 import { RefreshTokenRepository } from '@auth/infra/typeorm/repositories/refresh-token.repository';
 import { RevokedToken } from './infra/typeorm/entities/revoked-token.entity';
@@ -29,6 +29,7 @@ import { RefreshToken } from './infra/typeorm/entities/refresh-token.entity';
     LocalStrategy,
     JwtStrategy,
     FindByEmail,
+    FindById,
     RefreshTokenRepository,
     RevokedTokenRepository,
   ],
