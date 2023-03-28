f54e7c25eb1f2cbd7488edaad7bd8c58572d9ebc
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-03-28 14:40:04 -0300

    docs: adds property to filter transaction by status

--- server/src/transactions/dto/filter-transaction-by-status.dto.ts
+++ server/src/transactions/dto/filter-transaction-by-status.dto.ts
@@ -1,9 +1,15 @@
 import { IsEnum, IsNotEmpty } from 'class-validator';
 
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Status } from '@shared/constants';
 
 export class FilterTransactionsByStatusDTO {
   @IsNotEmpty()
   @IsEnum(Status)
+  @ApiPropertyOptional({
+    example: 'PENDING',
+    description: 'Status em que a transação está no momento',
+    type: 'Status',
+  })
   status: Status;
 }
