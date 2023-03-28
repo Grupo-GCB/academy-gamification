ba3736f4bfdcee8947c5a78cc73bc752985aec41
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-27 23:02:50 -0300

    test: mocks and tests sendgridservice

--- server/src/users/use-cases/registerUser/register-user.spec.ts
+++ server/src/users/use-cases/registerUser/register-user.spec.ts
@@ -1,14 +1,33 @@
+import { SendGridService } from '@anchan828/nest-sendgrid';
+import { MailService } from '@sendgrid/mail';
+
 import { BusinessUnits } from '@shared/constants';
 import { InMemoryUsersRepository } from '@users/test/in-memory/inMemoryUserRepository';
 import { RegisterUser } from './register-user';
 
+jest.mock('@anchan828/nest-sendgrid', () => {
+  return {
+    SendGridService: jest.fn().mockImplementation(() => {
+      return {
+        send: jest.fn().mockImplementation(() => Promise.resolve()),
+        mailService: new MailService(),
+      };
+    }),
+  };
+});
+
 describe('Register user', () => {
   let inMemoryUsersRepository: InMemoryUsersRepository;
   let sut: RegisterUser;
+  let sendGridMock: SendGridService;
 
   beforeEach(() => {
     inMemoryUsersRepository = new InMemoryUsersRepository();
-    sut = new RegisterUser(inMemoryUsersRepository);
+    sendGridMock = new SendGridService(
+      { apikey: 'fake-api-key' },
+      new MailService(),
+    );
+    sut = new RegisterUser(inMemoryUsersRepository, sendGridMock);
   });
 
   it('should be able to register an user', async () => {
@@ -35,5 +54,14 @@ describe('Register user', () => {
         business_unit: BusinessUnits.ADIANTE,
       }),
     ).rejects.toThrow('Invalid email');
+  });
+
+  it('should call sendGrid.send with correct arguments', async () => {
+    const email = 'gustavo.wuelta@gcbinvestimentos.com';
+    const business_unit = BusinessUnits.ADIANTE;
+
+    await sut.execute({ email, business_unit });
+
+    expect(sendGridMock.send).toHaveBeenCalled();
   });
 });
