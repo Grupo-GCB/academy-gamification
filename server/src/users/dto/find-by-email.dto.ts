a2accdaea45dcdbf9db207ba64a71dcd88e877cb
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-03-29 19:51:10 -0300

    feat: adds message to validate if email is empty or invalid

--- server/src/users/dto/find-by-email.dto.ts
+++ server/src/users/dto/find-by-email.dto.ts
@@ -2,8 +2,8 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class FindByEmailDTO {
-  @IsEmail()
-  @IsNotEmpty()
+  @IsEmail({}, { message: 'O endereço de e-mail informado é inválido!' })
+  @IsNotEmpty({ message: 'Insira um endereço de e-mail!' })
   @ApiProperty({
     example: 'john.doe@gmail.com',
     description: 'E-mail do usuário',
