d01af6b34b352af396aee5dd842d53758faaed28
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-03-29 17:33:24 -0300

    fix: removes duplicate decorators

--- server/src/transactions/dto/filter-transaction-by-status.dto.ts
+++ server/src/transactions/dto/filter-transaction-by-status.dto.ts
@@ -6,8 +6,6 @@ import { Status } from '@shared/constants';
 export class FilterTransactionsByStatusDTO {
   @IsNotEmpty()
   @IsEnum(Status)
-  @IsNotEmpty()
-  @IsEnum(Status)
   @ApiProperty({
     example: 'PENDING',
     description: 'Status em que a transação será filtrada',
