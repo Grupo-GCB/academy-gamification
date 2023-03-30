0145ffc5ec12e64276db3dd56a1216c74649548b
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-03-30 10:14:02 -0300

    feat: validate if id is invalid

--- server/src/transactions/use-cases/findById/find-by-id.ts
+++ server/src/transactions/use-cases/findById/find-by-id.ts
@@ -1,4 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { isUuidValid } from '@shared/utils';
 
 import { Transaction } from '@transactions/infra/typeorm/entities/transaction.entity';
 import { ITransactionsRepository } from '@transactions/interfaces';
@@ -8,11 +13,13 @@ export class FindById {
   constructor(private transactionsRepository: ITransactionsRepository) {}
 
   async execute(id: string): Promise<Transaction> {
+    if (!isUuidValid(id)) throw new BadRequestException('Id inválido!');
+
     const transaction: Transaction = await this.transactionsRepository.findById(
       id,
     );
 
-    if (!transaction) throw new NotFoundException('Transação não existe!');
+    if (!transaction) throw new NotFoundException('Transação não encontrada!');
 
     return transaction;
   }
