13f363ad82bd2bdcd0f97d6fe4f566038c8047a8
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-30 09:38:17 -0300

    feat: validates if token is revoged

--- server/src/auth/guards/jwt-auth.guard.ts
+++ server/src/auth/guards/jwt-auth.guard.ts
@@ -7,20 +7,31 @@ import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 
 import { IS_PUBLIC_KEY } from '@auth/decorators';
+import { RevokedTokenRepository } from '@auth/infra/typeorm/repositories/revoked-token.repository';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(private reflector: Reflector) {
+  constructor(
+    private reflector: Reflector,
+    private revokedTokenRepository: RevokedTokenRepository,
+  ) {
     super();
   }
 
-  canActivate(context: ExecutionContext): Promise<boolean> | boolean {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
 
     if (isPublic) return true;
+
+    const req = context.switchToHttp().getRequest();
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (token && (await this.revokedTokenRepository.isTokenRevoked(token))) {
+      throw new UnauthorizedException('Token revogado!');
+    }
 
     const canActivate = super.canActivate(context);
 
