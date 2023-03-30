1c789a1f30a3b3b2a7b807db8269932729bbb034
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-30 15:59:51 -0300

    chore: exports dto

--- server/src/transactions/dto/index.ts
+++ server/src/transactions/dto/index.ts
@@ -2,3 +2,4 @@ export * from './filter-transaction-by-status.dto';
 export * from './filter-transactions-by-user.dto';
 export * from './register-transaction.dto';
 export * from './update-status-dto';
+export * from './find-latest-transaction-by-status-and-subtype';
