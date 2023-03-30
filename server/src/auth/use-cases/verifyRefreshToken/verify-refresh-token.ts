25673f787e7f91d3a1fe07e85bb0d74e135d2575
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-30 19:37:20 -0300

    chore: passes IRefreshTokenInterface as parameter type

--- server/src/auth/use-cases/verifyRefreshToken/verify-refresh-token.ts
+++ server/src/auth/use-cases/verifyRefreshToken/verify-refresh-token.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
-import { RefreshTokenRepository } from '@auth/infra/typeorm/repositories';
+import { IRefreshTokenRepository } from '@auth/interfaces';
 
 @Injectable()
 export class VerifyRefreshToken {
-  constructor(private refreshTokenRepository: RefreshTokenRepository) {}
+  constructor(private refreshTokenRepository: IRefreshTokenRepository) {}
 
   async execute(token: string): Promise<boolean> {
     const refreshToken = await this.refreshTokenRepository.findRefreshToken(
