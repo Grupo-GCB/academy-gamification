0748c71ca7a8c6b4de702fd92b139d05463cba57
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-03-31 15:37:06 -0300

    test: refactors test

--- server/src/auth/use-cases/validateUser/validate-user.spec.ts
+++ server/src/auth/use-cases/validateUser/validate-user.spec.ts
@@ -7,21 +7,21 @@ import { User } from '@users/infra/entities';
 describe('ValdiateUser', () => {
   let inMemoryUserRepository: InMemoryUsersRepository;
   let sut: ValidateUser;
+  let user: User;
 
   beforeEach(async () => {
     inMemoryUserRepository = new InMemoryUsersRepository();
     sut = new ValidateUser(inMemoryUserRepository);
-  });
-
-  it('should return a user if email and password are correct', async () => {
-    const user: User = await inMemoryUserRepository.create({
+    user = await inMemoryUserRepository.create({
       name: 'Levi Ciarrochi',
       email: 'levi.ciarrochi@gcbinvestimentos.com',
       password: 'gcb123',
       business_unit: BusinessUnits.ADIANTE,
       role: Roles.COLLABORATOR,
     });
+  });
 
+  it('should return a user if email and password are correct', async () => {
     const validatedUser = await sut.execute(
       'levi.ciarrochi@gcbinvestimentos.com',
       'gcb123',
@@ -35,28 +35,12 @@ describe('ValdiateUser', () => {
   });
 
   it('should throw an error if there is no user with passed email', async () => {
-    const user: User = await inMemoryUserRepository.create({
-      name: 'Levi Ciarrochi',
-      email: 'levi.ciarrochi@gcbinvestimentos.com',
-      password: 'gcb123',
-      business_unit: BusinessUnits.ADIANTE,
-      role: Roles.COLLABORATOR,
-    });
-
     await expect(
       sut.execute('vitor.freitas@gcbinvestimentos.com', 'gcb123'),
     ).rejects.toThrow(new Error('Endereço de e-mail ou senha incorretos'));
   });
 
   it('should throw an error if password is incorrect', async () => {
-    const user: User = await inMemoryUserRepository.create({
-      name: 'Levi Ciarrochi',
-      email: 'levi.ciarrochi@gcbinvestimentos.com',
-      password: 'gcb123',
-      business_unit: BusinessUnits.ADIANTE,
-      role: Roles.COLLABORATOR,
-    });
-
     await expect(
       sut.execute('levi.ciarrochi@gcbinvestimentos.com', 'gcbola123'),
     ).rejects.toThrow(new Error('Endereço de e-mail ou senha incorretos'));
